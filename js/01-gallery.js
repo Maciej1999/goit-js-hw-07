9d34e4b6b86cba3edb337cb43b9e32a3e1f06d6a
Maciej1999 <maciej.sikora.1999@wp.pl>
2023-01-17 22:56:23 +0100

    End of hw-js-07

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+for (let i = 0; i < galleryItems.length; i++) {
+  const div = document.createElement("div");
+  div.className = "gallery__item";
+  const link = document.createElement("a");
+  link.className = "gallery__link";
+  link.href = `${galleryItems[i].original}`;
+  const img = document.createElement("img");
+  img.className = "gallery__image";
+  img.src = galleryItems[i].preview;
+  img.dataset.source = galleryItems[i].original;
+  img.alt = galleryItems[i].description;
+  link.appendChild(img);
+  div.appendChild(link);
+  gallery.appendChild(div);
+}
+gallery.addEventListener("click", (event) => {
+  event.stopPropagation();
+  event.preventDefault();
+  const instance = basicLightbox.create(
+    `
+  <img src="${event.target.dataset.source}">
+  `
+  );
+  instance.show();
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+for (let i = 0; i < galleryItems.length; i++) {
+  const link = document.createElement("a");
+  link.className = "gallery__item";
+  link.href = `${galleryItems[i].original}`;
+  const img = document.createElement("img");
+  img.className = "gallery__image";
+  img.src = galleryItems[i].preview;
+  img.alt = galleryItems[i].description;
+  link.appendChild(img);
+  gallery.appendChild(link);
+}
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
